fa306fa1953bb06dec7565e4017afc85108e98e9
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2025-10-27 09:05:21 +0100

    initial_project_setup

---
+++ src/main/java/com/company/Document/Management/System/Demo/DocumentManagementSystemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.company.Document.Management.System.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementSystemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementSystemDemoApplication.class, args);
+	}
+
+}
